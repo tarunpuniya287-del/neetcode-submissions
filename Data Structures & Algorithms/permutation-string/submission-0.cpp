cdff9ecc1bff3a11a36bc5399ce8ba91518a7895
tarunpuniya287-del <tarunpuniya287@gmail.com>
2026-07-17 13:04:19 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+        sort(s1.begin(), s1.end());
+        for (int i = 0; i <= s2.size() - s1.size(); i++) {
+            string sub = s2.substr(i, s1.size());
+            sort(sub.begin(), sub.end());
+            if (sub == s1) return true;
+        }
+        return false;
+    }
+};
